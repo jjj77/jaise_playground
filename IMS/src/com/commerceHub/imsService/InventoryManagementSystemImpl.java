700d7c0635344e8669c039ddc4455f6886c60144
jjj77 <j.jaise1@gmail.com>
2016-11-11 08:52:37 -0500

    modified

--- IMS/src/com/commerceHub/imsService/InventoryManagementSystemImpl.java
+++ IMS/src/com/commerceHub/imsService/InventoryManagementSystemImpl.java
@@ -6,6 +6,9 @@ import com.commerceHub.dataSource.MockDatabase;
 import com.commerceHub.model.Product;
 
 public class InventoryManagementSystemImpl implements InventoryManagementSystem {
+	
+	private String messageForPicking = "{0} units of Product {1} bearing ID {2} picked";
+	private String messageForRestocking = "{0} units of Product {1} bearing ID {2} added";
 	ConcurrentMap<String, Product> productList = MockDatabase.getList();
 
 	@Override
@@ -14,11 +17,12 @@ public class InventoryManagementSystemImpl implements InventoryManagementSystem 
 			Product productX = (Product) productList.get(productId);
 			if (productX != null) {
 				if (amountToPick > productX.getInventoryLevel()) {
-					System.out.println("Not enough products");
+					System.out.println("Not sufficient units of product "+productX.getProductID()+" in the inventory");
 				} else {
 					int temp = productX.getInventoryLevel() - amountToPick;
 					productX.setInventoryLevel(temp);
 					productList.replace(productId, productX);
+					System.out.println(amountToPick+" units of product "+productX.getProductID()+" removed from the inventory. Inventory level = "+productX.getInventoryLevel());
 				}
 			} else {
 				System.out.println("the product id is invalid");
@@ -36,11 +40,13 @@ public class InventoryManagementSystemImpl implements InventoryManagementSystem 
 			Product productX = (Product) productList.get(productId);
 			if (productX != null) {
 				if ((amountToRestock + productX.getInventoryLevel()) > productX.getMaxLevel()) {
-					System.out.println("Over the limit. Only "+(productX.getMaxLevel()-productX.getInventoryLevel())+" units can be added");
+					System.out.println("Over the limit. Only " + (productX.getMaxLevel() - productX.getInventoryLevel())
+							+ " units of "+productX.getProductID()+" can be added more");
 				} else {
 					int temp = productX.getInventoryLevel() + amountToRestock;
 					productX.setInventoryLevel(temp);
 					productList.replace(productId, productX);
+					System.out.println(amountToRestock+" units of product "+productX.getProductID()+" added to the inventory. Inventory level = "+productX.getInventoryLevel());
 				}
 			} else {
 				System.out.println("the product id is invalid");
